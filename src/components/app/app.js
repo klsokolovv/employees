64cb87cbc68888fd2081c9855366c9bd8b2b8a95
Kirill Sokolov <klsokolovv@mail.ru>
2022-05-17 21:23:11 +0300

    Initial comment

---
+++ src/components/app/app.js
@@ -0,0 +1,31 @@
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-pannel/search-pannel';
+import AppFilter from '../app-filter/app-filter';
+import EmployersList from '../employers-list/employers-list';
+import EmployersAddForm from '../employers-add-form/employers-add-form';
+import './app.css';
+
+function App() {
+
+    const data = [
+        {name: 'John C.', salary: 800, increase: false, id: 1},
+        {name: 'Alex M.', salary: 3000, increase: false, id: 2},
+        {name: 'Carl W.', salary: 5000, increase: false, id: 3}
+    ];
+
+    return (
+        <div className="app">
+            <AppInfo/>
+
+            <div className="search-pannel">
+                <SearchPanel/>
+                <AppFilter/>
+            </div>
+
+            <EmployersList data = {data}/>
+            <EmployersAddForm/>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/employers-list/employers-list.js
@@ -0,0 +1,20 @@
+import EmployersListItem from '../employers-list-item/employers-list-item';
+import './employers-list.css';
+
+const EmployersList = ({data}) => {
+
+    const elements = data.map(item  => {
+        return (
+            // <EmployersListItem name ={item.name} salary = {item.salary}/>
+            <EmployersListItem {...item}/>
+        )
+    })
+
+    return (
+        <ul className="app-list list-group">
+            {elements}
+        </ul>
+    )
+}
+
+export default EmployersList;
